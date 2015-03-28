17638ea98eac1c18a08c5cf105073f2241964ec0
jun7 <jun7@hush.com>
2015-03-28 14:48:17 +0900

    Modestly window close

--- ROX-Filer/src/filer.c
+++ ROX-Filer/src/filer.c
@@ -725,6 +725,9 @@ gboolean filer_window_delete(GtkWidget *window,
 	if (mount)
 		may_offer_unmount(filer_window, mount);
 
+	/* Without this, the window will flash more */
+	gdk_window_resize(window->window, 4, 4);
+
 	return FALSE;
 }
 
